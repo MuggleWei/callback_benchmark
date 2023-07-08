5c4209515fc7d185ea34662a325bd6f943e23e6a
Muggle Wei <mugglewei@gmail.com>
2023-07-08 22:11:10 +0800

    update scripts

--- perfstat.sh
+++ perfstat.sh
@@ -4,3 +4,7 @@ perf stat -B -e \
 	task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,branches,branch-misses,cache-misses ./bin/example_hello f
 perf stat -B -e \
 	task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,branches,branch-misses,cache-misses ./bin/example_hello v
+perf stat -B -e \
+	task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,branches,branch-misses,cache-misses ./bin/example_hello F
+perf stat -B -e \
+	task-clock,context-switches,cpu-migrations,page-faults,cycles,instructions,branches,branch-misses,cache-misses ./bin/example_hello V
--- pprof.sh
+++ pprof.sh
@@ -11,3 +11,15 @@ env LD_PRELOAD="/usr/lib/libprofiler.so" \
 	CPUPROFILE=./hello_v.prof \
 	./bin/example_hello v
 pprof --text ./bin/example_hello ./hello_v.prof
+
+env LD_PRELOAD="/usr/lib/libprofiler.so" \
+	CPUPROFILE_FREQUENCY=10000 \
+	CPUPROFILE=./hello_ef.prof \
+	./bin/example_hello F
+pprof --text ./bin/example_hello ./hello_ef.prof
+
+env LD_PRELOAD="/usr/lib/libprofiler.so" \
+	CPUPROFILE_FREQUENCY=10000 \
+	CPUPROFILE=./hello_ev.prof \
+	./bin/example_hello V
+pprof --text ./bin/example_hello ./hello_ev.prof
